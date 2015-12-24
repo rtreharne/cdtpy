592d70ed6dab49aaee87dbdc09cf8aa1525b0170
treharne <R.Treharne@liverpool.ac.uk>
2015-12-24 08:37:32 +0000

    Induction.py script added

---
+++ 001/Induction.py
@@ -0,0 +1,19 @@
+#pylab combines pyplot with numpy into a single namespace. This is convenient for interactive work, but for programming it is recommended that the namespaces be kept separate
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Objective 1
+print  "{0:.2f}".format(np.pi)
+
+#Objective 2
+
+x = np.arange(0, 5*np.pi, 0.1)
+y = np.sin(x)
+
+plt.plot(x,y)
+plt.xlabel('x (radians)')
+plt.ylabel('sin(x)')
+plt.title('My first plot using a python script!')
+
+plt.show() #You need to include this to show the plot
