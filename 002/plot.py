08b2beb3cd1c023f932d3cb94010a64503bc7598
treharne <R.Treharne@liverpool.ac.uk>
2015-12-26 19:29:59 +0000

    plot.py tweak

--- 002/plot.py
+++ 002/plot.py
@@ -8,20 +8,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import csv
+import os
 
-def plot_file(filename):
-    f = open(filename, 'rb')
-    reader = csv.reader(f)
-    headers = reader.next()
+def plot_file(path, delim=','):
+    abspath = os.path.abspath(path) #get absolute path
+    f = open(abspath, 'rb') #open file for reading
+    header = csv.Sniffer().has_header(f.read(1024)) #check for headers
+    f.seek(0) #return to start of file
+    reader = csv.reader(f, delimiter=delim)
+    
+    if header:
+        #get header text
+        header_text = reader.next()
+
+    #read data to list
     data = []
     for line in reader:
         data.append(line)
+
+    #transpose data into columns
     data = [[float(y) for y in x] for x in np.transpose(data)]
-    plt.plot(data[0], data[1], '-o')
-    plt.xlabel(headers[0])
-    plt.ylabel(headers[1])
+
+    #create plot
+    plot = plt.plot(data[0], data[1], '-o')
+
+    #if file has headers use these as label axes
+    if header:
+        plt.xlabel(header_text[0])
+        plt.ylabel(header_text[1])
+    return plot
 
 if __name__ == "__main__":
-    plot_file(sys.argv[1])
-    plt.show()
+    plot = plot_file(sys.argv[1])
+    plt.show(plot)
 
