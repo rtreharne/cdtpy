1821d3dcd339ad09b7a58c6ae290b785cd689c3c
treharne <R.treharne@liverpool.ac.uk>
2017-01-17 11:35:30 +0000

    002 plot.py and .ipynb converted to python3

--- 002/plot.py
+++ 002/plot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #Mission 002 | Lock and Load
 #Autor | Robert Treharne
@@ -14,32 +14,36 @@ import matplotlib as mpl
 mpl.rcParams['axes.labelsize'] = 'x-large'
 
 def plot_file(path, delim=','):
+
     abspath = os.path.abspath(path) #get absolute path
-    f = open(abspath, 'rb') #open file for reading
-    header = csv.Sniffer().has_header(f.read(1024)) #check for headers
-    f.seek(0) #return to start of file
-    reader = csv.reader(f, delimiter=delim)
+
+    sniffer = csv.Sniffer()
+
+    with open(abspath, 'r') as f:
+        has_header = sniffer.has_header(f.read(1024))
+        f.seek(0)
+        reader = csv.reader(f, delimiter=delim)
     
-    if header:
-        #get header text
-        header_text = reader.next()
+        if has_header:
+            #get header text
+            header_text = next(reader)
 
-    #read data to list
-    data = []
-    for line in reader:
-        data.append(line)
+        #read data to list
+        data = []
+        for line in reader:
+            data.append(line)
 
-    #transpose data into columns
-    data = [[float(y) for y in x] for x in np.transpose(data)]
+        #transpose data into columns
+        data = [[float(y) for y in x] for x in np.transpose(data)]
 
-   #create plot
-    plot = plt.plot(data[0], data[1], '-o')
+       #create plot
+        plot = plt.plot(data[0], data[1], '-o')
 
-    #if file has headers use these as label axes
-    if header:
-        plt.xlabel(header_text[0])
-        plt.ylabel(header_text[1])
-    return plot
+        #if file has headers use these as label axes
+        if has_header:
+            plt.xlabel(header_text[0])
+            plt.ylabel(header_text[1])
+        return plot
 
 if __name__ == "__main__":
     plot = plot_file(sys.argv[1])
